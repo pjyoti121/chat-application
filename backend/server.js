fb6484cc83cc502222230207a0f31e98c3f6c861
=pjyoti121 <jyotipandey20.setmodyuniversity.ac.in>
2022-08-02 19:54:22 +0530

    chat-appli

---
+++ backend/server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const { chats } = require("./data/data");
+
+
+const app = express();
+
+app.get("/", (req,res) => {
+    res.send("API is Running");
+
+});
+
+app.get("/api/chat",(req, res) => {
+  res.send(chats);
+    
+});
+
+app.listen(5000, console.log("Server started at port 5000"));
